41b7ba190d4e54735d3dfa04029611f7f80785bc
thefattahi <fattahi@mail.de>
2026-08-13 05:15:33 +0200

    Clear captions for all photos
    Removed captions from multiple photo entries.

--- photos.js
+++ photos.js
@@ -2,25 +2,25 @@ const photos = [
 
   {
     file: "photo-01.jpg",
-    caption: "Late afternoon",
+    caption: "",
     category: "Street"
   },
 
   {
     file: "photo-02.jpg",
-    caption: "Between places",
+    caption: "",
     category: "Street"
   },
 
   {
     file: "photo-03.jpg",
-    caption: "Quiet morning",
+    caption: "",
     category: "Portrait"
   },
 
   {
     file: "photo-04.jpg",
-    caption: "After the rain",
+    caption: "",
     category: "Documentary"
   }
 
