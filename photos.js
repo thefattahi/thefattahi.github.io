71df13dfd46bee62b515f34177d10781ad2a8677
thefattahi <fattahi@mail.de>
2026-08-13 05:01:22 +0200

    Add initial photos data array

---
+++ photos.js
@@ -0,0 +1,27 @@
+const photos = [
+
+  {
+    file: "photo-01.jpg",
+    caption: "Late afternoon",
+    category: "Street"
+  },
+
+  {
+    file: "photo-02.jpg",
+    caption: "Between places",
+    category: "Street"
+  },
+
+  {
+    file: "photo-03.jpg",
+    caption: "Quiet morning",
+    category: "Portrait"
+  },
+
+  {
+    file: "photo-04.jpg",
+    caption: "After the rain",
+    category: "Documentary"
+  }
+
+];
