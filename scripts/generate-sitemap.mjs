a1613d11a54924a891098372c69161c11c247622
thefattahi <fattahi@mail.de>
2026-09-07 04:44:40 +0200

    Add automated image sitemap generator

---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,28 @@
+import fs from "node:fs";
+import vm from "node:vm";
+
+const site = "https://thefattahi.github.io";
+const source = fs.readFileSync("photos.js", "utf8");
+const context = {};
+vm.runInNewContext(`${source}\nresult = photos;`, context);
+const photos = Array.isArray(context.result) ? context.result : [];
+
+const escapeXml = (value) => String(value)
+  .replaceAll("&", "&amp;")
+  .replaceAll("<", "&lt;")
+  .replaceAll(">", "&gt;")
+  .replaceAll('"', "&quot;")
+  .replaceAll("'", "&apos;");
+
+const today = new Date().toISOString().slice(0, 10);
+const images = photos.map((photo) => {
+  const loc = `${site}/images/${encodeURI(photo.file)}`;
+  const title = photo.title || photo.alt || "Photograph by Rasool Fattahi";
+  const caption = photo.caption || photo.description || photo.alt || title;
+  return `    <image:image>\n      <image:loc>${escapeXml(loc)}</image:loc>\n      <image:title>${escapeXml(title)}</image:title>\n      <image:caption>${escapeXml(caption)}</image:caption>\n    </image:image>`;
+}).join("\n");
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n  <url>\n    <loc>${site}/</loc>\n    <lastmod>${today}</lastmod>\n${images}\n  </url>\n</urlset>\n`;
+
+fs.writeFileSync("sitemap.xml", sitemap);
+console.log(`Generated sitemap for ${photos.length} photos.`);
