41b74c65b5bb0da2e66e629e8680785b6ab39333
thefattahi <fattahi@mail.de>
2026-09-10 19:05:20 +0200

    Make sitemap generator preserve multilingual and photo detail URLs

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -15,6 +15,7 @@ const escapeXml = (value) => String(value)
   .replaceAll("'", "&apos;");
 
 const today = new Date().toISOString().slice(0, 10);
+
 const images = photos.map((photo) => {
   const loc = `${site}/images/${encodeURI(photo.file)}`;
   const title = photo.title || photo.alt || "Photograph by Rasool Fattahi";
@@ -22,7 +23,12 @@ const images = photos.map((photo) => {
   return `    <image:image>\n      <image:loc>${escapeXml(loc)}</image:loc>\n      <image:title>${escapeXml(title)}</image:title>\n      <image:caption>${escapeXml(caption)}</image:caption>\n    </image:image>`;
 }).join("\n");
 
-const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n  <url>\n    <loc>${site}/</loc>\n    <lastmod>${today}</lastmod>\n${images}\n  </url>\n</urlset>\n`;
+const photoPages = photos.map((photo) => {
+  const slug = photo.file.replace(/\.[^.]+$/, "");
+  return `  <url>\n    <loc>${site}/photos/${encodeURIComponent(slug)}.html</loc>\n    <lastmod>${today}</lastmod>\n  </url>`;
+}).join("\n");
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n  <url>\n    <loc>${site}/</loc>\n    <lastmod>${today}</lastmod>\n${images}\n  </url>\n  <url>\n    <loc>${site}/fa/</loc>\n    <lastmod>${today}</lastmod>\n  </url>\n${photoPages}\n</urlset>\n`;
 
 fs.writeFileSync("sitemap.xml", sitemap);
-console.log(`Generated sitemap for ${photos.length} photos.`);
+console.log(`Generated sitemap for ${photos.length} photos plus the Persian archive and photo detail pages.`);
